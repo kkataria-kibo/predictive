2ce3468644a27534e9c9c1bd443a1d8ba2c30d2e
kkataria-kibo <kkataria@marketlive.com>
2016-08-27 16:45:59 -0700

    spring security and template
    spring security and template

--- src/main/java/com/scuti/predictive/PredictiveApplication.java
+++ src/main/java/com/scuti/predictive/PredictiveApplication.java
@@ -6,13 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
 @EnableMongoRepositories("com.scuti.predictive.repository")
-public class PredictiveApplication {
+public class PredictiveApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PredictiveApplication.class, args);
+	}
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/home").setViewName("home");
 	}
 }
